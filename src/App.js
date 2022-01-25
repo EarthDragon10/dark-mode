aaac49864e3d9d58becb9ed0638358593ae7bc2a
Laguda Stefano <lagudadev@gmail.com>
2022-01-25 18:25:11 +0100

    feat: added logic js for change theme

--- src/App.js
+++ src/App.js
@@ -2,11 +2,38 @@ import { useState, useEffect } from "react";
 import data from "./data";
 import Article from "./Article";
 
+const getFromLocalStorage = () => {
+  if (localStorage.getItem("theme")) {
+    return localStorage.getItem("theme");
+  } else {
+    return "light-mode";
+  }
+};
+
 function App() {
+  const [theme, setTheme] = useState(getFromLocalStorage());
+
+  const changeTheme = () => {
+    if (theme === "light-mode") {
+      setTheme("dark-mode");
+    } else {
+      setTheme("light-mode");
+    }
+  };
+
+  console.log(theme);
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
   return (
     <section className="section-center">
       <div className="container">
-        <button className="btn">Cambia</button>
+        <button className="btn" onClick={changeTheme}>
+          Cambia
+        </button>
         <section className="article-section">
           {data.map((el) => {
             return <Article key={el.id} {...el} />;
