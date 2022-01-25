6f91bb955e3c4e0d390ec26e9e6b6abe98456161
Laguda Stefano <lagudadev@gmail.com>
2022-01-25 16:07:42 +0100

    feat: added basic structure of site

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import data from "./data";
+import Article from "./Articolo";
+
+function App() {
+  return (
+    <section className="section-center">
+      <div className="container">
+        <h2>DARK MODE APP</h2>
+      </div>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/Article.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Article = () => {
+  return (
+    <article>
+      <h6>Articolo</h6>
+    </article>
+  );
+};
+
+export default Article;
